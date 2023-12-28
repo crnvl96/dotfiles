52869bca330429a502fa156fb990d965f1532794
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-28 15:58:03 -0300

    feat: Adds `oil.nvim` plugin

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,27 @@
+return {
+    {
+        "stevearc/oil.nvim",
+        lazy = false,
+        opts = {
+            delete_to_trash = true,
+            lsp_rename_autosave = true,
+            constrain_cursor = "name",
+            keymaps = {
+                ["<C-h>"] = false,
+                ["<C-l>"] = false,
+                ["g;"] = "actions.toggle_trash",
+            },
+            view_options = {
+                show_hidden = true,
+            },
+            preview = {
+                win_options = {
+                    winblend = 100,
+                },
+            },
+        },
+        keys = {
+            { "<M-o>", "<cmd>Oil<CR>" },
+        },
+    },
+}
