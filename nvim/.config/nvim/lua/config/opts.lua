c309f7629ae507ff5a1b58842df953724ded9a30
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-29 14:28:10 -0300

    Disabled some unused providers

--- nvim/.config/nvim/lua/config/opts.lua
+++ nvim/.config/nvim/lua/config/opts.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 
