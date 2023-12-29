7c17ffe38477a36079f07a429e76acccd88cc611
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-29 14:59:04 -0300

    refact: small formatting on `conform`

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -7,6 +7,7 @@ return {
         end,
         opts = {
             notify_on_error = false,
+            format_on_save = { timeout_ms = 1000, lsp_fallback = true },
             formatters_by_ft = {
                 fish = { "fish_indent" },
                 lua = { "stylua" },
@@ -16,7 +17,6 @@ return {
                 typescript = { "prettierd" },
                 typescriptreact = { "prettierd" },
             },
-            format_on_save = { timeout_ms = 1000, lsp_fallback = true },
         },
     },
 }
