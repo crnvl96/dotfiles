f3fcb72c9c3b034f68ddf484abb19d0d342db991
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-29 02:15:37 -0300

    refact: Adds confg for nvim.surround

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -3,7 +3,21 @@ return {
         "kylechui/nvim-surround",
         event = "VeryLazy",
         config = function()
-            require("nvim-surround").setup()
+            require("nvim-surround").setup({
+                keymaps = {
+                    insert = false,
+                    insert_line = false,
+                    normal_line = false,
+                    normal_cur_line = false,
+                    visual_line = false,
+                    change_line = false,
+                    delete = "dz",
+                    change = "cz",
+                    visual = "Z",
+                    normal = "yz",
+                    normal_cur = "yzz",
+                },
+            })
         end,
     },
 }
