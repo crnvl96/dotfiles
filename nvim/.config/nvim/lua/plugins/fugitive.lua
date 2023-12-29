0af86c2431464652664ec408a675b8f311b4443c
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-29 02:19:34 -0300

    feat: pretty git log

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -5,5 +5,10 @@ return {
         { "<leader>gx", "<cmd>Git mergetool --name-status<CR>", desc = "Open All Conflicts in QF" },
         { "<leader>gc", "<cmd>Git commit <bar> wincmd J<CR>", desc = "Commit" },
         { "<leader>gq", "<cmd>Git difftool --name-status<CR>", desc = "Open All Diffs in QF" },
+        {
+            "<leader>gl",
+            "<cmd>Git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit<CR>",
+            desc = "Log",
+        },
     },
 }
