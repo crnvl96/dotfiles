307daaa009b6614d1528d47a46ea391afce284dd
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-30 01:25:52 -0300

    feat: adds gh theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,37 @@
 return {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    init = function()
-        vim.opt.background = "dark"
-        vim.g.gruvbox_material_background = "hard"
-        vim.g.gruvbox_material_enable_bold = 1
-        vim.g.gruvbox_material_enable_italic = 1
-        vim.g.gruvbox_material_sign_column_background = "grey"
-        vim.g.gruvbox_material_spell_foreground = "colored"
-        vim.g.gruvbox_material_diagnostic_text_highlight = 1
-        vim.g.gruvbox_material_diagnostic_line_highlight = 1
-        vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-        vim.g.gruvbox_material_current_word = "underline"
-        vim.g.gruvbox_material_better_performance = 1
+    {
+        "sainnhe/gruvbox-material",
+        lazy = false,
+        priority = 1000,
+        enabled = false,
+        init = function()
+            vim.opt.background = "dark"
+            vim.g.gruvbox_material_background = "hard"
+            vim.g.gruvbox_material_enable_bold = 1
+            vim.g.gruvbox_material_enable_italic = 1
+            vim.g.gruvbox_material_sign_column_background = "grey"
+            vim.g.gruvbox_material_spell_foreground = "colored"
+            vim.g.gruvbox_material_diagnostic_text_highlight = 1
+            vim.g.gruvbox_material_diagnostic_line_highlight = 1
+            vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+            vim.g.gruvbox_material_current_word = "underline"
+            vim.g.gruvbox_material_better_performance = 1
 
-        vim.cmd.colorscheme("gruvbox-material")
-    end,
+            vim.cmd.colorscheme("gruvbox-material")
+        end,
+    },
+    {
+        "projekt0n/github-nvim-theme",
+        lazy = false,
+        priority = 1000,
+        enabled = true,
+        init = function()
+            vim.opt.background = "dark"
+        end,
+        config = function()
+            require("github-theme").setup({})
+
+            vim.cmd("colorscheme github_dark_dimmed")
+        end,
+    },
 }
