a01bfe243917cc1f1a1fc7587301f8788ed1145f
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-30 20:09:27 -0300

    refact: adds overseer as a debug dependency

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -7,53 +7,43 @@ return {
         { "mxsdev/nvim-dap-vscode-js" },
         { "rcarriga/nvim-dap-ui" },
         { "jbyuki/one-small-step-for-vimkind" },
-    },
-    -- stylua: ignore
-    keys = {
-        {"<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Breakpoint Condition"},
-        {"<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint"},
-        {"<leader>dc", function() require("dap").continue() end, desc = "Continue"},
-        ---@diagnostic disable-next-line: undefined-global
-        {"<leader>da", function() require("dap").continue({before = get_args}) end, desc = "Run with Args"},
-        {"<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor"},
-        {"<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)"},
-        {"<leader>di", function() require("dap").step_into() end, desc = "Step Into"},
-        {"<leader>dj", function() require("dap").down() end, desc = "Down"},
-        {"<leader>dk", function() require("dap").up() end, desc = "Up"},
-        {"<leader>dl", function() require("dap").run_last() end, desc = "Run Last"},
-        {"<leader>do", function() require("dap").step_out() end, desc = "Step Out"},
-        {"<leader>dO", function() require("dap").step_over() end, desc = "Step Over"},
-        {"<leader>dp", function() require("dap").pause() end, desc = "Pause"},
-        {"<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL"},
-        {"<leader>ds", function() require("dap").session() end, desc = "Session"},
-        {"<leader>dt", function() require("dap").terminate() end, desc = "Terminate"},
-
-        -- dapui
-        {"<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets"},
-        {"<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI"},
-        {"<leader>de", function() require("dapui").eval() require("dapui").eval() end, desc = "Evaluate expression", mode = {"n", "v"}},
-
-        -- Lua adapter
-        {"<leader>dl", function() require("osv").launch({port = 8086}) end, desc = "Launch Lua adapter"},
-        {"<leader>dn", function() require("neotest").run.run({strategy = "dap"}) end, desc = "Debug nearest (other langs)"},
-
+        { "stevearc/overseer.nvim" },
     },
     config = function()
+        require("overseer").setup({
+            dap = false,
+            templates = { "builtin", "user.run_file" },
+            task_list = {
+                default_detail = 2,
+                direction = "bottom",
+                max_width = { 600, 0.7 },
+                bindings = {
+                    ["<C-b>"] = "ScrollOutputUp",
+                    ["<C-f>"] = "ScrollOutputDown",
+                    ["<M-p>"] = "TogglePreview",
+                    ["H"] = "DecreaseAllDetail",
+                    ["L"] = "IncreaseAllDetail",
+                    -- Disable mappings I don't use.
+                    ["g?"] = false,
+                    ["<C-l>"] = false,
+                    ["<C-h>"] = false,
+                    ["{"] = false,
+                    ["}"] = false,
+                },
+            },
+        })
         require("dap-go").setup()
         require("nvim-dap-virtual-text").setup({ virt_text_pos = "eol" })
         require("dap-vscode-js").setup({
             debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
             adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
         })
-
         require("overseer").patch_dap(true)
         require("dap.ext.vscode").json_decode = require("overseer.json").decode
-
         require("dapui").setup()
         require("dap").listeners.after.event_initialized["dapui_config"] = function()
             require("dapui").open()
         end
-
         require("dap").adapters.nlua = function(callback, config)
             callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
         end
@@ -69,4 +59,37 @@ return {
             ["pwa-node"] = { "typescript", "javascript" },
         })
     end,
+    -- stylua: ignore
+    keys = {
+        -- overseer
+        { "<leader>ot", "<cmd>OverseerToggle<cr>", desc = "Toggle task window" },
+        { "<leader>or", "<cmd>OverseerRun<cr>", desc = "Run task" },
+
+        -- Dap
+        { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, desc = "Breakpoint Condition" },
+        { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+        { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+        ---@diagnostic disable-next-line: undefined-global
+        { "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+        { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+        { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+        { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+        { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+        { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+        { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+        { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+        { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+        { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+        { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+        { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+        { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+
+        -- dapui
+        { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+        { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+        {"<leader>de", function() require("dapui").eval() require("dapui").eval() end, desc = "Evaluate expression", mode = {"n", "v"}},
+
+        -- Lua adapter
+        {"<leader>dl", function() require("osv").launch({port = 8086}) end, desc = "Launch Lua adapter"},
+    },
 }
