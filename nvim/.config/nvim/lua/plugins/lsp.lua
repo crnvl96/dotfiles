030dd7e3af05b79d70aa44d5a64f904b70e6c069
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-30 20:13:18 -0300

    refact: new rules for cmp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -17,6 +17,10 @@ return {
                     { "hrsh7th/cmp-buffer" },
                     { "L3MON4D3/LuaSnip" },
                     { "windwp/nvim-autopairs" },
+                    { "hrsh7th/cmp-path" },
+                    { "hrsh7th/cmp-nvim-lua" },
+                    { "rcarriga/cmp-dap" },
+                    { "hrsh7th/cmp-nvim-lsp-signature-help" },
                 },
             },
         },
@@ -446,12 +450,29 @@ return {
                 },
             })
 
+            vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
             local cmp = require("cmp")
             local lspkind = require("lspkind")
+            local luasnip = require("luasnip")
+
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
 
             cmp.setup({
+                enabled = function()
+                    ---@diagnostic disable-next-line: deprecated
+                    return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+                end,
                 preselect = cmp.PreselectMode.None,
                 sorting = require("cmp.config.default")().sorting,
+                experimental = {
+                    ghost_text = {
+                        hl_group = "CmpGhostText",
+                    },
+                },
                 formatting = {
                     format = lspkind.cmp_format({
                         mode = "text_symbol",
@@ -461,14 +482,6 @@ return {
                             return vim_item
                         end,
                     }),
-                    -- format = function(entry, vim_item)
-                    --     vim_item.kind = ""
-                    --     vim_item.menu = ({
-                    --         buffer = "(Buffer)",
-                    --         nvim_lsp = "(LSP)",
-                    --     })[entry.source.name]
-                    --     return vim_item
-                    -- end,
                 },
                 completion = {
                     completeopt = "menu,menuone,noinsert,noselect",
@@ -485,17 +498,54 @@ return {
                 },
                 snippet = {
                     expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
+                        luasnip.lsp_expand(args.body)
                     end,
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Replace }),
+                    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Replace }),
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                    ["<CR>"] = cmp.mapping({
+                        i = function(fallback)
+                            if cmp.visible() and cmp.get_active_entry() then
+                                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                            else
+                                fallback()
+                            end
+                        end,
+                        s = cmp.mapping.confirm({ select = true }),
+                        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+                    }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                        -- that way you will only jump inside the snippet region
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
                 }),
                 sources = cmp.config.sources({
+                    { name = "nvim_lsp_signature_help" },
+                    { name = "nvim_lua" },
                     {
                         name = "nvim_lsp",
                         entry_filter = function(entry)
@@ -509,15 +559,15 @@ return {
                             return true
                         end,
                     },
+                    { name = "path" },
                 }, {
                     { name = "buffer" },
                 }),
             })
 
-            cmp.setup.cmdline({ "/", "?" }, {
-                mapping = cmp.mapping.preset.cmdline(),
+            require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
                 sources = {
-                    { name = "buffer" },
+                    { name = "dap" },
                 },
             })
 
