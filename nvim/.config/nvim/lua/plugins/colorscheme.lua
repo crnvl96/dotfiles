395862b96f6ce837df51bcf7d70d2d85f9cce497
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-30 02:00:11 -0300

    gh colors for nvim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -29,9 +29,18 @@ return {
             vim.opt.background = "dark"
         end,
         config = function()
-            require("github-theme").setup({})
+            require("github-theme").setup({
+                options = {
+                    transparent = false,
+                    styles = {
+                        comments = "italic",
+                        keywords = "bold",
+                        types = "italic,bold",
+                    },
+                },
+            })
 
-            vim.cmd("colorscheme github_dark_dimmed")
+            vim.cmd("colorscheme github_dark_high_contrast")
         end,
     },
 }
