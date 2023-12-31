242a2e967a510c4ee73962fed4e9ef57a637d8df
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-31 13:00:30 -0300

    adds clues and event prop to neotest

--- nvim/.config/nvim/lua/plugins/test.lua
+++ nvim/.config/nvim/lua/plugins/test.lua
@@ -1,48 +1,54 @@
 return {
-    {
-        "nvim-neotest/neotest",
-        dependencies = {
-            { "antoinemadec/FixCursorHold.nvim" },
-            { "nvim-neotest/neotest-plenary" },
-            { "nvim-neotest/neotest-go" },
-            { "nvim-neotest/neotest-jest" },
-            { "nvim-neotest/neotest-vim-test" },
-            { "vim-test/vim-test" },
-        },
-        config = function()
-            local neotest_ns = vim.api.nvim_create_namespace("neotest")
-            vim.diagnostic.config({
-                virtual_text = {
-                    format = function(diagnostic)
-                        local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-                        return message
-                    end,
-                },
-            }, neotest_ns)
+    "nvim-neotest/neotest",
+    event = "VeryLazy",
+    dependencies = {
+        { "antoinemadec/FixCursorHold.nvim" },
+        { "nvim-neotest/neotest-plenary" },
+        { "nvim-neotest/neotest-go" },
+        { "nvim-neotest/neotest-jest" },
+        { "nvim-neotest/neotest-vim-test" },
+        { "vim-test/vim-test" },
+    },
+    config = function()
+        local clue = require("mini.clue")
+        clue.config.clues = vim.list_extend(clue.config.clues, {
+            { mode = "n", keys = "<leader>t", desc = "+test" },
+            { mode = "x", keys = "<leader>t", desc = "+test" },
+        })
 
-            require("neotest").setup({
-                adapters = {
-                    require("neotest-plenary"),
-                    require("neotest-go"),
-                    require("neotest-jest")({
-                        jestCommand = "npm test --",
-                        jestConfigFile = "custom.jest.config.ts",
-                        env = { CI = true },
-                        cwd = function() -- path
-                            return vim.fn.getcwd()
-                        end,
-                    }),
-                    require("neotest-vim-test")({ ignore_filetypes = { "go", "javascript", "typescript", "jsx", "tsx" } }),
-                },
-                status = { virtual_text = true },
-                output = { open_on_run = true },
-                quickfix = {
-                    open = function()
-                        vim.cmd("copen")
+        local neotest_ns = vim.api.nvim_create_namespace("neotest")
+        vim.diagnostic.config({
+            virtual_text = {
+                format = function(diagnostic)
+                    local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+                    return message
+                end,
+            },
+        }, neotest_ns)
+
+        require("neotest").setup({
+            adapters = {
+                require("neotest-plenary"),
+                require("neotest-go"),
+                require("neotest-jest")({
+                    jestCommand = "npm test --",
+                    jestConfigFile = "custom.jest.config.ts",
+                    env = { CI = true },
+                    cwd = function() -- path
+                        return vim.fn.getcwd()
                     end,
-                },
-            })
-        end,
+                }),
+                require("neotest-vim-test")({ ignore_filetypes = { "go", "javascript", "typescript", "jsx", "tsx" } }),
+            },
+            status = { virtual_text = true },
+            output = { open_on_run = true },
+            quickfix = {
+                open = function()
+                    vim.cmd("copen")
+                end,
+            },
+        })
+    end,
         -- stylua: ignore
         keys = {
           { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
@@ -53,5 +59,4 @@ return {
           { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
           { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
         },
-    },
 }
