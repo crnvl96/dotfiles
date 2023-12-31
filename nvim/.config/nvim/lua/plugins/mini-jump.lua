493e5284c9d1e5e2a16d35274e60fe48d09d94f2
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-31 01:10:48 -0300

    new plugind for hopping around

---
+++ nvim/.config/nvim/lua/plugins/mini-jump.lua
@@ -0,0 +1,13 @@
+return {
+    "echasnovski/mini.jump",
+    event = "VeryLazy",
+    opts = {
+        mappings = {
+            forward = "f",
+            backward = "F",
+            forward_till = "t",
+            backward_till = "T",
+            repeat_jump = ";",
+        },
+    },
+}
---
+++ nvim/.config/nvim/lua/plugins/mini-jump2d.lua
@@ -0,0 +1,19 @@
+return {
+    "echasnovski/mini.jump2d",
+    event = "VeryLazy",
+    opts = {
+        labels = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        view = {
+            dim = true,
+            n_steps_ahead = 0,
+        },
+        allowed_windows = {
+            current = true,
+            not_current = false,
+        },
+        mappings = {
+            start_jumping = "s",
+        },
+        silent = false,
+    },
+}
