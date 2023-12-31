6a86505d246de8d2bbf65eec63b61542c0c33e29
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2023-12-31 12:59:17 -0300

    removes non generic clues from mini clue file

--- nvim/.config/nvim/lua/plugins/mini-clue.lua
+++ nvim/.config/nvim/lua/plugins/mini-clue.lua
@@ -12,14 +12,6 @@ return {
                 end,
             })
 
-            vim.api.nvim_create_autocmd("FileType", {
-                group = vim.api.nvim_create_augroup("crnvl96_enable_clue_triggers_fugitive", { clear = true }),
-                pattern = { "fugitive" },
-                callback = function(data)
-                    miniclue.enable_buf_triggers(data.buf)
-                end,
-            })
-
             -- Add a-z/A-Z marks.
             local function mark_clues()
                 local marks = {}
@@ -78,148 +70,57 @@ return {
 
             return {
                 triggers = {
-                    -- Builtins.
+                    { mode = "n", keys = "<Leader>" },
+                    { mode = "x", keys = "<Leader>" },
+
+                    -- Built-in completion
+                    { mode = "i", keys = "<C-x>" },
+
+                    -- `g` key
                     { mode = "n", keys = "g" },
                     { mode = "x", keys = "g" },
+
+                    -- Marks
+                    { mode = "n", keys = "'" },
                     { mode = "n", keys = "`" },
+                    { mode = "x", keys = "'" },
                     { mode = "x", keys = "`" },
+
+                    -- Registers
                     { mode = "n", keys = '"' },
                     { mode = "x", keys = '"' },
                     { mode = "i", keys = "<C-r>" },
                     { mode = "c", keys = "<C-r>" },
+
+                    -- Window commands
                     { mode = "n", keys = "<C-w>" },
+
+                    -- `z` key
                     { mode = "n", keys = "z" },
-                    -- Leader triggers.
-                    { mode = "n", keys = "<leader>" },
-                    { mode = "x", keys = "<leader>" },
-                    -- Moving between stuff.
+                    { mode = "x", keys = "z" },
+
+                    -- next/prev
                     { mode = "n", keys = "[" },
                     { mode = "n", keys = "]" },
-                    -- surround
-                    { mode = "n", keys = "y" },
-                    { mode = "n", keys = "d" },
-                    { mode = "n", keys = "c" },
-                    -- textobjects
-                    { mode = "x", keys = "i" },
-                    { mode = "x", keys = "a" },
                 },
                 clues = {
-                    { mode = "n", keys = "<leader>b", desc = "+buffers" },
-                    { mode = "n", keys = "<leader>c", desc = "+code" },
-                    { mode = "x", keys = "<leader>c", desc = "+code" },
-                    { mode = "n", keys = "<leader>d", desc = "+dap" },
-                    { mode = "x", keys = "<leader>d", desc = "+dap" },
-                    { mode = "n", keys = "<leader>f", desc = "+find" },
-                    { mode = "x", keys = "<leader>f", desc = "+find" },
-                    { mode = "n", keys = "<leader>g", desc = "+git" },
-                    { mode = "x", keys = "<leader>g", desc = "+git" },
-                    { mode = "n", keys = "<leader>o", desc = "+overseer" },
-                    { mode = "n", keys = "<leader>s", desc = "+search" },
-                    { mode = "x", keys = "<leader>s", desc = "+search" },
-                    { mode = "x", keys = "<leader>t", desc = "+test" },
-                    { mode = "n", keys = "<leader>w", desc = "+windows" },
+                    -- next/prev clues
                     { mode = "n", keys = "[", desc = "+prev" },
                     { mode = "n", keys = "]", desc = "+next" },
+
+                    -- leader clues
+                    { mode = "n", keys = "<leader>b", desc = "+buffers" },
+                    { mode = "n", keys = "<leader>w", desc = "+windows" },
+
                     -- Useful builtins.
+                    miniclue.gen_clues.builtin_completion(),
+                    miniclue.gen_clues.g(),
+                    miniclue.gen_clues.windows(),
+                    miniclue.gen_clues.z(),
                     miniclue.gen_clues.marks(),
                     miniclue.gen_clues.registers(),
                     mark_clues,
                     macro_clues,
-                    -- Clues for g mappings.
-                    { mode = "n", keys = "g'", desc = "Jump to mark (don't affect jumplist)" },
-                    { mode = "n", keys = "g*", desc = "Search word under cursor" },
-                    { mode = "n", keys = "gE", desc = "Go backwards to end of previous WORD" },
-                    { mode = "n", keys = "gT", desc = "Go to previous tabpage" },
-                    { mode = "n", keys = "g`", desc = "Jump to mark (don't affect jumplist)" },
-                    { mode = "n", keys = "ga", desc = "Print ascii value" },
-                    { mode = "n", keys = "ge", desc = "Go backwards to end of previous word" },
-                    { mode = "n", keys = "gg", desc = "Go to line (def: first)" },
-                    { mode = "n", keys = "gq", desc = "Format text (operator)" },
-                    { mode = "n", keys = "gt", desc = "Go to next tabpage" },
-                    { mode = "n", keys = "gx", desc = "Execute app for file under cursor" },
-                    -- Clues for z mappings.
-                    { mode = "n", keys = "z=", desc = "Show spelling suggestions" },
-                    { mode = "n", keys = "zA", desc = "Toggle folds recursively" },
-                    { mode = "n", keys = "zC", desc = "Close folds recursively" },
-                    { mode = "n", keys = "zF", desc = "Create fold" },
-                    { mode = "n", keys = "zM", desc = "Close all folds" },
-                    { mode = "n", keys = "zO", desc = "Open folds recursively" },
-                    { mode = "n", keys = "zR", desc = "Open all folds" },
-                    { mode = "n", keys = "za", desc = "Toggle fold" },
-                    { mode = "n", keys = "zb", desc = "Redraw at bottom" },
-                    { mode = "n", keys = "zc", desc = "Close fold" },
-                    { mode = "n", keys = "zf", desc = "Create fold (operator)" },
-                    { mode = "n", keys = "zi", desc = "Toggle 'foldenable'" },
-                    { mode = "n", keys = "zj", desc = "Move to start of next fold" },
-                    { mode = "n", keys = "zk", desc = "Move to end of previous fold" },
-                    { mode = "n", keys = "zm", desc = "Fold more" },
-                    { mode = "n", keys = "zo", desc = "Open fold" },
-                    { mode = "n", keys = "zr", desc = "Fold less" },
-                    { mode = "n", keys = "zt", desc = "Redraw at top" },
-                    { mode = "n", keys = "zv", desc = "Open enough folds" },
-                    { mode = "n", keys = "zx", desc = "Update folds + open enough folds" },
-                    -- Clues for window mappings.
-                    { mode = "n", keys = "<C-w>=", desc = "Make windows same dimensions" },
-                    { mode = "n", keys = "<C-w>F", desc = "Split + edit file name + jump" },
-                    { mode = "n", keys = "<C-w>H", desc = "Move to very left" },
-                    { mode = "n", keys = "<C-w>J", desc = "Move to very bottom" },
-                    { mode = "n", keys = "<C-w>K", desc = "Move to very top" },
-                    { mode = "n", keys = "<C-w>L", desc = "Move to very right" },
-                    { mode = "n", keys = "<C-w>R", desc = "Rotate up/left" },
-                    { mode = "n", keys = "<C-w>c", desc = "Close" },
-                    { mode = "n", keys = "<C-w>f", desc = "Split + edit file name" },
-                    { mode = "n", keys = "<C-w>n", desc = "Open new" },
-                    { mode = "n", keys = "<C-w>o", desc = "Close all but current" },
-                    { mode = "n", keys = "<C-w>q", desc = "Quit current" },
-                    { mode = "n", keys = "<C-w>r", desc = "Rotate down/right" },
-                    { mode = "n", keys = "<C-w>s", desc = "Split horizontally" },
-                    { mode = "n", keys = "<C-w>v", desc = "Split vertically" },
-                    { mode = "n", keys = "<C-w>x", desc = "Exchange windows" },
-                    -- textobjects
-                    { mode = "x", keys = "if", desc = "Inside function" },
-                    { mode = "x", keys = "io", desc = "Inside block" },
-                    { mode = "x", keys = "ic", desc = "Inside class" },
-                    { mode = "x", keys = "it", desc = "Inside tag" },
-
-                    { mode = "x", keys = "af", desc = "Around function" },
-                    { mode = "x", keys = "ao", desc = "Around block" },
-                    { mode = "x", keys = "ac", desc = "Around class" },
-                    { mode = "x", keys = "at", desc = "Around tag" },
-
-                    { mode = "x", keys = "i(", desc = "Inside ()" },
-                    { mode = "x", keys = "i)", desc = "Inside () w/space" },
-                    { mode = "x", keys = "i[", desc = "Inside []" },
-                    { mode = "x", keys = "i]", desc = "Inside [] w/space" },
-                    { mode = "x", keys = "i{", desc = "Inside {}" },
-                    { mode = "x", keys = "i}", desc = "Inside {} w/ space" },
-                    { mode = "x", keys = "i<", desc = "Inside <> w/ space" },
-                    { mode = "x", keys = "i>", desc = "Inside <> w/ space" },
-                    { mode = "x", keys = "ib", desc = "Inside <{[()]}> w/ space" },
-
-                    { mode = "x", keys = "a(", desc = "Around ()" },
-                    { mode = "x", keys = "a)", desc = "Around () w/space" },
-                    { mode = "x", keys = "a[", desc = "Around []" },
-                    { mode = "x", keys = "a]", desc = "Around [] w/space" },
-                    { mode = "x", keys = "a{", desc = "Around {}" },
-                    { mode = "x", keys = "a}", desc = "Around {} w/ space" },
-                    { mode = "x", keys = "a<", desc = "Around <>" },
-                    { mode = "x", keys = "a>", desc = "Around <> w/ space" },
-                    { mode = "x", keys = "ab", desc = "Around <{[()]}> w/ space" },
-
-                    { mode = "x", keys = "i'", desc = "Inside '" },
-                    { mode = "x", keys = 'i"', desc = 'Inside "' },
-                    { mode = "x", keys = "i`", desc = "Inside `" },
-                    { mode = "x", keys = "iq", desc = "Inside '`\"" },
-
-                    { mode = "x", keys = "a'", desc = "Around '" },
-                    { mode = "x", keys = 'a"', desc = 'Around "' },
-                    { mode = "x", keys = "a`", desc = "Around `" },
-                    { mode = "x", keys = "aq", desc = "Around '`\"" },
-
-                    { mode = "x", keys = "i?", desc = "Inside prompt" },
-                    { mode = "x", keys = "a?", desc = "Around prompt" },
-
-                    { mode = "n", keys = "dv", postkeys = "d", desc = "(Fugitive diff)" },
                 },
                 window = {
                     delay = 200,
