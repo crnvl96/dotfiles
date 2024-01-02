d245b85a32397850748e996ae41058a4c49d6358
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-01-02 11:29:58 -0300

    colorscheme update

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,25 +5,8 @@ return {
         lazy = false,
         priority = 1000,
         enabled = true,
-        opts = {
-            groups = {
-                background = "#151515",
-                background_nc = "#151515",
-            },
-            highlight_groups = {
-                StatusLine = { fg = "love", bg = "love", blend = 10 },
-                StatusLineNC = { fg = "subtle", bg = "surface" },
-                CursorLine = { bg = "overlay", blend = 50 },
-                Visual = { bg = "love", blend = 10 },
-                MatchParen = { fg = "love", bg = "#151515" },
-                LspReferenceText = { bg = "pine", blend = 50 },
-                LspReferenceRead = { bg = "pine", blend = 50 },
-                LspReferenceWrite = { bg = "pine", blend = 50 },
-            },
-        },
-        config = function(_, opts)
+        init = function()
             vim.opt.background = "dark"
-            require("rose-pine").setup(opts)
             vim.cmd("colorscheme rose-pine")
         end,
     },
