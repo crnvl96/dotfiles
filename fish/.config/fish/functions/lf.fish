1297f7c64c2c44e14da9849e100e96c1a0b8690f
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-01-02 20:44:09 -0300

    lf settings

--- fish/.config/fish/functions/lf.fish
+++ fish/.config/fish/functions/lf.fish
@@ -1,26 +1,6 @@
-function lf_cd
+function lf
     set LFCD (command lf -print-last-dir $argv)
     if test -d "$LFCD"
         cd $LFCD
-    end
-end
-
-
-function lf
-    set path
-    set clipboard_content (fish_clipboard_paste)
-
-    if test -n "$clipboard_content"
-        set path $clipboard_content
-    else
-        set path $argv
-    end
-
-    set LFCD (command lf -print-last-dir $path)
-
-    if test -d "$LFCD"
-        cd $LFCD
-    else
-        lf_cd
     end
 end
