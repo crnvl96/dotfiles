4a355c8e3a3e9d60f1ea6543e99cd340eab8f8a1
Adran Carnavale <adran@hey.com>
2025-06-27 17:18:05 -0300

    feat(term): install tmux

--- install/3-terminal.sh
+++ install/3-terminal.sh
@@ -1,6 +1,6 @@
 yay -S --noconfirm --needed \
   wget curl unzip inetutils \
-  eza fzf ripgrep zoxide \
+  eza fzf ripgrep zoxide tmux \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate \
   alacritty
