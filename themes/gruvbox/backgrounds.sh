62cce6bb262c2ba681baa8afea87abfab8db6edd
Adran Carnavale <adran@hey.com>
2025-06-29 16:24:51 -0300

    update styles

--- themes/gruvbox/backgrounds.sh
+++ themes/gruvbox/backgrounds.sh
@@ -1,2 +1,3 @@
 mkdir -p "$BACKGROUNDS_DIR/gruvbox"
 download_background_image "https://github.com/basecamp/omakub/raw/refs/heads/master/themes/gruvbox/background.jpg" "gruvbox/1-grubox.jpg"
+download_background_image "https://images.unsplash.com/photo-1502790671504-542ad42d5189?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" "gruvbox/2-grubox.jpg"
