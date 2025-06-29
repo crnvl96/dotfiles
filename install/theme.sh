0cfcc9cf928cd6c73083dd3814a4848238180b5b
Adran Carnavale <adran@hey.com>
2025-06-29 01:39:37 -0300

    update default theme

--- install/theme.sh
+++ install/theme.sh
@@ -12,10 +12,10 @@ for f in ~/.local/share/omarchy/themes/*; do ln -s "$f" ~/.config/omarchy/themes
 
 # Set initial theme
 mkdir -p ~/.config/omarchy/current
-ln -snf ~/.config/omarchy/themes/tokyo-night ~/.config/omarchy/current/theme
-source ~/.local/share/omarchy/themes/tokyo-night/backgrounds.sh
-ln -snf ~/.config/omarchy/backgrounds/tokyo-night ~/.config/omarchy/current/backgrounds
-ln -snf ~/.config/omarchy/current/backgrounds/1-Pawel-Czerwinski-Abstract-Purple-Blue.jpg ~/.config/omarchy/current/background
+ln -snf ~/.config/omarchy/themes/everforest ~/.config/omarchy/current/theme
+source ~/.local/share/omarchy/themes/everforest/backgrounds.sh
+ln -snf ~/.config/omarchy/backgrounds/everforest ~/.config/omarchy/current/backgrounds
+ln -snf ~/.config/omarchy/current/backgrounds/1-everforest.jpg ~/.config/omarchy/current/background
 
 # Set specific app links for current theme
 ln -snf ~/.config/omarchy/current/theme/hyprlock.conf ~/.config/hypr/hyprlock.conf
