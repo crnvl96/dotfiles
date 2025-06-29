d7a2eb2a449a56047b6d3274ea73412e33b162d1
Adran Carnavale <adran@hey.com>
2025-06-29 16:41:42 -0300

    correct gruvbox bg names

--- themes/gruvbox/backgrounds.sh
+++ themes/gruvbox/backgrounds.sh
@@ -1,3 +1,3 @@
 mkdir -p "$BACKGROUNDS_DIR/gruvbox"
-download_background_image "https://github.com/basecamp/omakub/raw/refs/heads/master/themes/gruvbox/background.jpg" "gruvbox/1-grubox.jpg"
-download_background_image "https://gruvbox-wallpapers.pages.dev/wallpapers/irl/octopus.jpg" "gruvbox/2-grubox.jpg"
+download_background_image "https://github.com/basecamp/omakub/raw/refs/heads/master/themes/gruvbox/background.jpg" "gruvbox/1-gruvbox.jpg"
+download_background_image "https://gruvbox-wallpapers.pages.dev/wallpapers/irl/octopus.jpg" "gruvbox/2-gruvbox.jpg"
