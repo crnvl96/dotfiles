d6ec7d4239e802ec5fc82776967908077263d4fc
Adran Carnavale <adran@hey.com>
2025-06-30 22:19:10 -0300

    remove bin dir

--- install/xtras.sh
+++ install/xtras.sh
@@ -3,5 +3,12 @@ yay -S --noconfirm --needed \
   obsidian typora libreoffice obs-studio kdenlive \
   pinta xournalpp
 
-# Copy over Dotfiles applications
-source ~/.local/share/dotfiles/bin/dotfiles-sync-applications
+# Copy and sync icon files
+mkdir -p ~/.local/share/icons/hicolor/48x48/apps/
+cp ~/.local/share/dotfiles/applications/icons/*.png ~/.local/share/icons/hicolor/48x48/apps/
+gtk-update-icon-cache ~/.local/share/icons/hicolor &>/dev/null
+
+# Copy .desktop declarations
+mkdir -p ~/.local/share/applications
+cp ~/.local/share/dotfiles/applications/*.desktop ~/.local/share/applications/
+update-desktop-database ~/.local/share/applications
