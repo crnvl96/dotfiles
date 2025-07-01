c6124fd4cefe000d959f17f9daf2e53ae09036a8
Adran Carnavale <adran@hey.com>
2025-07-01 19:10:41 -0300

    uncomment section

--- install/19-xtras.sh
+++ install/19-xtras.sh
@@ -9,11 +9,11 @@ extra_packages=(
 install_packages "${extra_packages[@]}"
 
 msg "Installing custom application icons..."
-# mkdir -p ~/.local/share/icons/hicolor/48x48/apps/
-# cp ~/.local/share/dotfiles/applications/icons/*.png ~/.local/share/icons/hicolor/48x48/apps/
-# gtk-update-icon-cache ~/.local/share/icons/hicolor &>/dev/null
+mkdir -p ~/.local/share/icons/hicolor/48x48/apps/
+cp ~/.local/share/dotfiles/applications/icons/*.png ~/.local/share/icons/hicolor/48x48/apps/
+gtk-update-icon-cache ~/.local/share/icons/hicolor &>/dev/null
 
 msg "Installing custom .desktop files..."
-# mkdir -p ~/.local/share/applications
-# cp ~/.local/share/dotfiles/applications/*.desktop ~/.local/share/applications/
-# update-desktop-database ~/.local/share/applications
+mkdir -p ~/.local/share/applications
+cp ~/.local/share/dotfiles/applications/*.desktop ~/.local/share/applications/
+update-desktop-database ~/.local/share/applications
