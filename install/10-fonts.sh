76f5e959733afcb1209ffef1853f233f4f5033f2
Adran Carnavale <adran@hey.com>
2025-07-03 01:45:24 -0300

    remove berkeley mono font

--- install/10-fonts.sh
+++ install/10-fonts.sh
@@ -13,13 +13,6 @@ install_packages "${font_packages[@]}"
 msg "Installing custom fonts..."
 mkdir -p ~/.local/share/fonts
 
-if [ ! -d "$HOME/.local/share/fonts/Berkeley Mono" ]; then
-    msg "Linking Berkeley Mono font..."
-    ln -snf "$HOME/.local/share/dotfiles/fonts/Berkeley Mono" "$HOME/.local/share/fonts/"
-    # Update the font cache to make the new font available.
-    fc-cache -f
-fi
-
 if ! fc-list | grep -qi "CaskaydiaMono Nerd Font"; then
   cd /tmp
   wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
