8d753fd0031118e92e40c996f05d79a32d12f95b
Adran Carnavale <adran@hey.com>
2025-07-04 11:07:56 -0300

    waybar config

--- install/18.1-webapp.sh
+++ install/18.1-webapp.sh
@@ -1,41 +1,7 @@
-# Create a desktop launcher for a web app
-web2app() {
-  if [ "$#" -ne 3 ]; then
-    echo "Usage: web2app <AppName> <AppURL> <IconURL> (IconURL must be in PNG -- use https://dashboardicons.com)"
-    return 1
-  fi
-
-  local APP_NAME="$1"
-  local APP_URL="$2"
-  local ICON_URL="$3"
-  local ICON_DIR="$HOME/.local/share/applications/icons"
-  local DESKTOP_FILE="$HOME/.local/share/applications/${APP_NAME}.desktop"
-  local ICON_PATH="${ICON_DIR}/${APP_NAME}.png"
-
-  mkdir -p "$ICON_DIR"
-
-  if ! curl -sL -o "$ICON_PATH" "$ICON_URL"; then
-    echo "Error: Failed to download icon."
-    return 1
-  fi
-
-  cat > "$DESKTOP_FILE" <<EOF
-[Desktop Entry]
-Version=1.0
-Name=$APP_NAME
-Comment=$APP_NAME
-Exec=chromium --new-window --ozone-platform=wayland --app="$APP_URL" --name="$APP_NAME" --class="$APP_NAME"
-Terminal=false
-Type=Application
-Icon=$ICON_PATH
-StartupNotify=true
-EOF
-
-  chmod +x "$DESKTOP_FILE"
-}
-
+web2app "Excalidraw" https://excalidraw.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/excalidraw.png
 web2app "WhatsApp" https://web.whatsapp.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
 web2app "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 web2app "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
 web2app "Teams" https://teams.microsoft.com/v2/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/microsoft-teams.png
-web2app "Excalidraw" https://excalidraw.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/excalidraw.png
+web2app "Outlook" https://outlook.office.com/mail/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/microsoft-outlook.png
+web2app "JIRA" https://team-1611694015260.atlassian.net/jira/software/c/projects/OPD/boards/131/calendar https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/jira.png
